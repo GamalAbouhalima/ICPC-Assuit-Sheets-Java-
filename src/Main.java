149eaed4384bc6791e8a900a498bd092a4115446
Gamal-Abouhalima <gamalabdallah005@gmail.com>
2024-03-11 00:38:31 +0200

    Sheet_One

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    static void problem_A_Say_Hello(){
+        String s="Hello,";
+        String name;
+        Scanner scan=new Scanner(System.in);
+        name=scan.next();
+        System.out.println(s+" "+name);
+    }
+    public static void main(String[] args) {
+        problem_A_Say_Hello();
+
+
+    }
+}
