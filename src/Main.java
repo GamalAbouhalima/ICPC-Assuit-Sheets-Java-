2a8f541e583783a623026b52a582589a6a8cd587
Gamal-Abouhalima <gamalabdallah005@gmail.com>
2024-03-11 00:42:23 +0200

    Problem_A

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,25 @@ public class Main {
         name=scan.next();
         System.out.println(s+" "+name);
     }
+    static void problem_B_Basic_Data_Types(){
+        int a; long b;char c;float d;double e;
+        Scanner scan=new Scanner(System.in);
+        a=scan.nextInt();
+        System.out.println(a);
+        b=scan.nextLong();
+        System.out.println(b);
+        c=scan.next().charAt(0);
+        System.out.println(c);
+        d=scan.nextFloat();
+        System.out.println(d);
+        e=scan.nextDouble();
+        System.out.printf("%.1f",e);
+
+
+    }
     public static void main(String[] args) {
         problem_A_Say_Hello();
-
+        problem_B_Basic_Data_Types();
 
     }
 }
